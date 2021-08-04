0bae6405b152d1cdd4d6698dca40f8fdf398dde4
Aaron Stannard <aaron@petabridge.com>
2021-08-04 10:52:47 -0500

    Porting over code

---
+++ src/Petabridge.Cmd.Cluster.Sharding.Repair/ClusterShardingRepairCmd.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ClusterShardingRepairCmd.cs" company="Petabridge, LLC">
+//      Copyright (C) 2021 - 2021 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Petabridge.Cmd.Cluster.Sharding.Repair
+{
+    internal static class ClusterShardingRepairCmd
+    {
+        public static readonly CommandDefinition RemoveInternalClusterShardingData = new CommandDefinitionBuilder()
+            .WithName("delete-sharding-data")
+            .WithDescription("Lists all Akka.Cluster.Sharding regions known to the current node.")
+            .WithArgument(b => b.WithName("typeName")
+                .WithSwitch("-t").WithSwitch("-T").WithDescription("The name of the entity type.")
+                .AllowMultiple(true)
+                .IsMandatory(true))
+            .WithArgument(b => b.WithName("journalPluginId")
+                .WithSwitch("-j").WithSwitch("-J").WithDescription("The cluster sharding persistent journal plugin ID.")
+                .AllowMultiple(false)
+                .IsMandatory(false))
+            .WithArgument(b => b.WithName("snapshotPluginId")
+                .WithSwitch("-s").WithSwitch("-S").WithDescription("The cluster sharding persistent snapshot store plugin ID.")
+                .AllowMultiple(false)
+                .IsMandatory(false))
+            .Build();
+        
+        public static readonly CommandPalette ClusterShardingRepairCommandPalette = new CommandPalette("cluster-sharding-repair",
+            new[] {RemoveInternalClusterShardingData});
+    }
+}
---
+++ src/Petabridge.Cmd.Cluster.Sharding.Repair/ClusterShardingRepairCommands.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ClusterShardingRepairCommands.cs" company="Petabridge, LLC">
+//      Copyright (C) 2021 - 2021 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.Actor;
+using Petabridge.Cmd.Host;
+using static Petabridge.Cmd.Cluster.Sharding.Repair.ClusterShardingRepairCmd;
+
+namespace Petabridge.Cmd.Cluster.Sharding.Repair
+{
+    public class ClusterShardingRepairCommands : CommandPaletteHandler
+    {
+        public static ClusterShardingRepairCommands Instance = new ClusterShardingRepairCommands();
+        
+        private ClusterShardingRepairCommands() : base(ClusterShardingRepairCommandPalette)
+        {
+            HandlerProps = Props.Create(() => new ClusterShardingRepairCmdHandler());
+        }
+
+        public override Props HandlerProps { get; }
+
+    }
+}
